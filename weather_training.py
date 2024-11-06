61a31a29a34fec192f076391bf6849eeb9a6080c
archerlinn <archerlin0530@gmail.com>
2024-11-06 08:53:09 -0500

    Added data normalization and plot pred vs truth

--- weather_training.py
+++ weather_training.py
@@ -8,7 +8,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 from tqdm import tqdm
 import wandb
+import matplotlib.pyplot as plt
 from unet import UNet
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
 
 class WeatherDataset(Dataset):
     def __init__(self, data_files, transform=None):
@@ -21,13 +25,22 @@ class WeatherDataset(Dataset):
     def __getitem__(self, idx):
         data, _ = nrrd.read(self.data_files[idx])
         data = data[:, :, 0]
+        # Normalizing data to [0, 1]
         data_min, data_max = np.min(data), np.max(data)
         data = (data - data_min) / (data_max - data_min)
         data = torch.tensor(data, dtype=torch.float32).unsqueeze(0)
         target = data
         return data, target
 
-def train_model(model, train_loader, val_loader, num_epochs=10000, save_path='model_weights_epoch_latest.pth'):
+def plot_prediction_vs_ground_truth(prediction, ground_truth):
+    fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+    axes[0].imshow(ground_truth.squeeze().cpu().numpy(), cmap='viridis')
+    axes[0].set_title("Ground Truth")
+    axes[1].imshow(prediction.squeeze().cpu().numpy(), cmap='viridis')
+    axes[1].set_title("Prediction")
+    plt.show()
+
+def train_model(model, train_loader, val_loader, num_epochs=10000, save_path='/Users/archer/Downloads/weather_data/model_weights_epoch_latest.pth'):
     print("Training the model...")
     optimizer = optim.Adam(model.parameters(), lr=0.001)
     criterion = nn.MSELoss()
@@ -38,7 +51,7 @@ def train_model(model, train_loader, val_loader, num_epochs=10000, save_path='mo
         model.train()
         epoch_loss = 0
         for inputs, targets in tqdm(train_loader):
-            inputs, targets = inputs.to('cuda'), targets.to('cuda')
+            inputs, targets = inputs.to(DEVICE), targets.to(DEVICE)
             optimizer.zero_grad()
             outputs, _ = model(inputs)
             outputs = F.interpolate(outputs, size=targets.shape[2:], mode='bilinear', align_corners=False)
@@ -55,11 +68,14 @@ def train_model(model, train_loader, val_loader, num_epochs=10000, save_path='mo
         val_loss = 0
         with torch.no_grad():
             for val_inputs, val_targets in val_loader:
-                val_inputs, val_targets = val_inputs.to('cuda'), val_targets.to('cuda')
+                val_inputs, val_targets = val_inputs.to(DEVICE), val_targets.to(DEVICE)
                 val_outputs, _ = model(val_inputs)
                 val_outputs = F.interpolate(val_outputs, size=val_targets.shape[2:], mode='bilinear', align_corners=False)
                 loss = criterion(val_outputs, val_targets)
                 val_loss += loss.item()
+                
+                # Plot prediction vs ground truth for inspection
+                plot_prediction_vs_ground_truth(val_outputs[0], val_targets[0])
 
         avg_val_loss = val_loss / len(val_loader)
         val_loss_history.append(avg_val_loss)
@@ -81,7 +97,7 @@ def train_model(model, train_loader, val_loader, num_epochs=10000, save_path='mo
 if __name__ == "__main__":
     wandb.init(project="weather_forecast")
 
-    file_dir = '/content/drive/MyDrive/Weather_Model/CFD_Subset'
+    file_dir = '/Users/archer/Downloads/weather_data'
     data_files = [os.path.join(file_dir, f) for f in os.listdir(file_dir) if f.endswith(".nrrd")]
     dataset = WeatherDataset(data_files=data_files)
 
@@ -94,10 +110,10 @@ if __name__ == "__main__":
     val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
 
     model = UNet(in_channels=1, out_channels=1)
-    model = model.to('cuda')
+    model = model.to(DEVICE)
 
     # Load the latest checkpoint if available
-    checkpoint_path = '/content/drive/MyDrive/Weather_Model/model_weights_epoch_latest.pth'
+    checkpoint_path = '/Users/archer/Downloads/weather_data/model_weights_epoch_latest.pth'
     if os.path.exists(checkpoint_path):
         print(f"Loading model weights from {checkpoint_path}")
         checkpoint = torch.load(checkpoint_path)
